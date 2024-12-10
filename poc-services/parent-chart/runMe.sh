5d49dbf3034141e7ce288ef9b144f0c29f9b4ed9
Denis Tumakov <dmarshaltu@gmail.com>
2024-12-10 12:37:54 +0100

    second commit

--- poc-services/parent-chart/runMe.sh
+++ poc-services/parent-chart/runMe.sh
@@ -1,9 +1,8 @@
 # Build dependencies
-cd poc-services/parent-chart
 helm dependency build
 
 # Install the chart
-helm install poc ./parent-chart -n dev-weu
+helm install poc .  -n dev-weu
 
 # Watch the pods come up in order
 kubectl get pods -n dev-weu -w
