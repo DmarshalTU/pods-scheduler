9edecb5a690f23b04dd25362db0340d37b98fd99
Denis Tumakov (CodeWizard) <Denis.Tumakov@au10tix.com>
2024-12-11 13:42:29 +0200

    q

--- poc-services/parent-chart/runMe.sh
+++ poc-services/parent-chart/runMe.sh
@@ -2,7 +2,7 @@
 helm dependency build
 
 # Install the chart
-helm install poc .  -n dev-weu
+helm install poc .  -n denis-test
 
 # Watch the pods come up in order
-kubectl get pods -n dev-weu -w
+kubectl get pods -n denis-test -w
